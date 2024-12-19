cb6c151e9f8f5d3018a01b3384514860b2104ffc
Dmutre <104783173+Dmutre@users.noreply.github.com>
2024-12-19 21:51:27 +0100

    feat: added include for batch, added coverage

--- src/api/batch/batch.service.ts
+++ src/api/batch/batch.service.ts
@@ -69,6 +69,9 @@ export class BatchService {
 
     const batches = await this.batchRepo.findMany({
       where,
+      include: {
+        item: true,
+      },
       orderBy: {
         [orderBy]: orderDirection,
       },
